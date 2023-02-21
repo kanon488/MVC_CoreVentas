8d1013cae0ad6dfb0691414ac42683ed10dddb56
Jose Eduardo Ortiz Espinoza <jortize@icc.clavijero.edu.mx>
2023-02-21 17:04:39 -0600

    first commit

---
+++ Ventas.AplicacionWeb/Controllers/NegocioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ventas.AplicacionWeb.Controllers
+{
+    public class NegocioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ventas.IOC/Dependencia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ventas.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Ventas.DAL.Interfaces;
+using Ventas.DAL.Implementacion;
+using Ventas.BLL.Interfaces;
+using Ventas.BLL.Implementacion;
+
+namespace Ventas.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration) 
+        {
+            services.AddDbContext<DBVENTAContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultDB"));
+            });
+        }
+    }
+}
