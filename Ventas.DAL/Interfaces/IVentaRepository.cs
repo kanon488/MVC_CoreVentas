b5ae5b4732d680325d900d4c228b5dc52f7f3e63
José Eduardo Ortiz Espinoza <kanon488@hotmail.com>
2023-02-21 23:03:22 -0600

    Parte 4 min 18:23

---
+++ Ventas.DAL/Interfaces/IVentaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Entity;
+
+namespace Ventas.DAL.Interfaces
+{
+    public interface IVentaRepository : IGenericRepository<Venta>
+    {
+        Task<Venta> Registrar(Venta entidad);
+        Task<List<Venta>> Reporte(DateTime FechaInicio,DateTime FechaFin);
+    }
+}
